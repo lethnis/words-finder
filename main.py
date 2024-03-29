880112dab10849deaf1695bafbd4be6ffecd81e7
lethnis <88483002+lethnis@users.noreply.github.com>
2024-03-29 19:50:00 +0700

    added tab name and small info on top in main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import filters as F
 def main():
     """Main loop of the program. Creates a filter inputs and applies the to russian nouns"""
 
+    st.set_page_config(page_title="Искатор А-42", layout="wide", page_icon=":face_with_monocle:")
+
+    st.text("Программа для поиска слов по фильтрам. Исходный код и пример: https://github.com/lethnis/words-finder")
+
     # load all words into list
     words = open("russian_nouns.txt", encoding="utf-8").read().split("\n")
 
